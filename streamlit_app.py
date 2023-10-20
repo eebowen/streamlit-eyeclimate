dcd97017adc48b24b3de0276884938b13582bc8e
Bowen <nbzbws@gmail.com>
2023-10-19 22:39:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ from streamlit_image_comparison import image_comparison
 
 
 
-st.title("James Webb vs Hubble Telescope Pictures")
+# st.title("James Webb vs Hubble Telescope Pictures")
 
-st.markdown("# Southern Nebula")
+# st.markdown("# Southern Nebula")
 
 # render image-comparison
 image_comparison(
@@ -18,12 +18,12 @@ image_comparison(
 )
 
 
-st.markdown("# Galaxy Cluster SMACS 0723")
+# st.markdown("# Galaxy Cluster SMACS 0723")
 
-# render image-comparison
-image_comparison(
-    img1="https://www.webbcompare.com/img/hubble/deep_field_700.jpg",
-    img2="https://www.webbcompare.com/img/webb/deep_field_700.jpg",
-    label1="Hubble",
-    label2="Webb"
-)
+# # render image-comparison
+# image_comparison(
+#     img1="https://www.webbcompare.com/img/hubble/deep_field_700.jpg",
+#     img2="https://www.webbcompare.com/img/webb/deep_field_700.jpg",
+#     label1="Hubble",
+#     label2="Webb"
+# )
