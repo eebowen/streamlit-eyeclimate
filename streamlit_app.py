0673eb7ab4a8e3b9a63320a72469828e2667d208
Bowen <nbzbws@gmail.com>
2023-10-24 15:46:26 +0000

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from streamlit_juxtapose import juxtapose
 from PIL import Image
 import requests
 import pathlib
-'''
+
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -40,38 +40,15 @@ image_comparison(
     # starting_position=35
     # width=width
 )
-'''
 
 
 
-STREAMLIT_STATIC_PATH = (
-    pathlib.Path(st.__path__[0]) / "static"
-)  # at venv/lib/python3.9/site-packages/streamlit/static
 
-IMG1 = "slider2a.png"
-IMG2 = "slider2b.png"
+# STREAMLIT_STATIC_PATH = (
+#     pathlib.Path(st.__path__[0]) / "static"
+# )  # at venv/lib/python3.9/site-packages/streamlit/static
 
-DEFAULT_IMG1_URL = (
-    "https://juxtapose.knightlab.com/static/img/Sochi_11April2005.jpg"
-)
-DEFAULT_IMG2_URL = (
-    "https://juxtapose.knightlab.com/static/img/Sochi_22Nov2013.jpg"
-)
+# IMG1 = "slider2a.png"
+# IMG2 = "slider2b.png"
 
-def fetch_img_from_url(url: str) -> Image:
-    img = Image.open(requests.get(url, stream=True).raw)
-    return img
-
-# form = st.form(key="Image comparison")
-# img1_url = DEFAULT_IMG1_URL
-# img2_url = DEFAULT_IMG2_URL
-# # img1_url = form.text_input("Image one url", value=DEFAULT_IMG1_URL)
-# img1 = Image.open('slider2a')
-# img2 = Image.open('slider2a')
-
-# img1.save(STREAMLIT_STATIC_PATH / IMG1)
-
-# # img2_url = form.text_input("Image two url", value=DEFAULT_IMG2_URL)
-# img2 = fetch_img_from_url(img2_url)
-# img2.save(STREAMLIT_STATIC_PATH / IMG2)
-juxtapose(IMG1, IMG2)
+# juxtapose(IMG1, IMG2)
